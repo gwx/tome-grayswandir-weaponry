a39918bef9be5118bbe96298eae67505444a391a
gwx <garba1@localhost.localdomain>
2014-04-14 23:15:05 -0400

    Stores now have more items.

--- data/stores.lua
+++ data/stores.lua
@@ -1,4 +1,4 @@
-local filters, adds
+local filters, adds, store
 adds = {
   SWORD_WEAPON = 'rapier',
   AXE_WEAPON = {'spear', 'greatspear'},
@@ -11,7 +11,7 @@ adds = {
   ZIGUR_KNIFE_WEAPON = {'swordbreaker', 'whip', 'throwing knives',},}
 for store, weapons in pairs(adds) do
   if _G.type(weapons) ~= 'table' then weapons = {weapons} end
-  local store = loading_list[store].store
+  store = loading_list[store].store
   if _G.type(store.filters) == 'function' then
     store.filters = {store.filters()}
   end
@@ -19,8 +19,21 @@ for store, weapons in pairs(adds) do
   for _, subtype in pairs(weapons) do
     local filter = {type = 'weapon', subtype = subtype, id = true, tome_drops = 'store'}
     table.insert(store.filters, filter)
+    -- Increase the number of items to make up for the variety.
+    store.nb_fill = (store.nb_fill or 10) + 5
   end
 end
+
+store = loading_list.ANGOLWEN_STAFF_WAND.store
+store.nb_fill = 30
+store.filters = {
+  {type = 'weapon', subtype = 'staff', id = true, tome_drops = 'store',},
+  {type = 'weapon', subtype = 'aurastone', id = true, tome_drops = 'store',},
+  {type = 'charm', subtype = 'wand', id = true, tome_drops = 'store',},}
+store.post_filter = function(e)
+  return e.subtype == 'staff' or rng.percent(30)
+end
+
 
 filters = loading_list.ARENA_SHOP.store.filters
 adds = {'rapier', 'club', 'greatclub', 'swordbreaker', 'whip', 'trident', 'spear', 'greatspear', 'throwing knives', 'aurastone',}
