060cb5cb6491065180390f41a1a939df6c985af4
grayswandir <grayswandir76@gmail.com>
2014-09-26 02:05:01 -0400

    Minor fix.

--- data/talents/shield.lua
+++ data/talents/shield.lua
@@ -41,6 +41,7 @@ newTalent{
 		end
 	end,
 	on_pre_use = function(self, t, silent)
+		if not game.level then return true end
 		if not self:hasShield() then
 			if not silent then
 				game.logPlayer(self, "You require a weapon and a shield to use this talent.")
